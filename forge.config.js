241f00cd033d350a99fe03953e38d844e27f6618
Wondoo Kang <azyu@live.com>
2025-12-29 11:02:35 +0900

    chore: Add Electron Forge build configuration

---
+++ forge.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    packagerConfig: {},
+    rebuildConfig: {},
+    makers: [
+        {
+            name: '@electron-forge/maker-squirrel',
+            config: {
+                // authors, description, etc. can be added here
+            },
+        },
+        {
+            name: '@electron-forge/maker-zip',
+            platforms: ['darwin', 'win32'],
+        },
+        // {
+        //   name: '@electron-forge/maker-dmg',
+        //   config: {
+        //     format: 'ULFO'
+        //   }
+        // }
+    ],
+};
